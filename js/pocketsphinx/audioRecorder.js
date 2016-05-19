ac5b9efb7af80d7bdd7fe6e5a71c0c70f6cac2bf
Jasmeet Oberai <joberai@uwaterloo.ca>
2016-05-19 03:27:01 -0400

    a start

--- js/pocketsphinx/audioRecorder.js
+++ js/pocketsphinx/audioRecorder.js
@@ -1,5 +1,5 @@
 (function(window) {
-    var AUDIO_RECORDER_WORKER = 'js/audioRecorderWorker.js';
+    var AUDIO_RECORDER_WORKER = 'js/pocketsphinx/audioRecorderWorker.js';
     var AudioRecorder = function(source, cfg) {
 	this.consumers = [];
 	var config = cfg || {};
